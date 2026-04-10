7aa64c9421486223ea0a1ba003e5728e49759bea
keson3948 <petrkes@gmail.com>
2026-04-10 10:57:38 +0200

    Adjust QR code dimensions in pdf-label.blade.php

--- resources/views/pdf/pdf-label.blade.php
+++ resources/views/pdf/pdf-label.blade.php
@@ -36,7 +36,7 @@
     <table style="width: 100%; border-collapse: collapse; padding-top: 7px; padding-left: 5px;">
         <tr>
             <td style="width: 30%; vertical-align: middle; text-align: left; padding: 0; padding-right: 4px">
-                <img  src="data:image/svg+xml;base64,{{ $qrCode }}" style="width: 94px; height: 94px;" alt="QR">
+                <img  src="data:image/svg+xml;base64,{{ $qrCode }}" style="width: 89px; height: 89px;" alt="QR">
             </td>
             <td style="width: 70%; vertical-align: top; text-align: left; padding-left: 3px;">
                 <table style="width: 100%; border-collapse: collapse;">
